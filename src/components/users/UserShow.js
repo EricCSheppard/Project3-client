8b51c625ba20ea54022507d48147fd3967a6e598
AlexMcBex <alessiobelli94@gmail.com>
2023-02-14 11:13:54 -0500

    made base for usershow

---
+++ src/components/users/UserShow.js
@@ -0,0 +1,66 @@
+//import dependencies
+
+import { useState, useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import { Container, Card, Button } from 'react-bootstrap'
+import { getOneUser } from '../../api/user'
+import messages from '../shared/AutoDismissAlert/messages'
+import GoalsIndex from '../goals/GoalsIndex'
+
+// const goalCardContainerLayout = {
+//     display: 'flex',
+//     justifyContent: 'center',
+//     flexFlow: 'row wrap'
+// }
+
+
+//function component
+const ShowUser = (props) => {
+    //import props
+    const [profile, setProfile] = useState(null)
+    const { id } = useParams()
+    const { user, msgAlert } = props
+    console.log('user in UserShow props', user)
+    console.log('msgAlert in UserShow props', msgAlert)
+
+    useEffect(() => {
+        getOneUser(id)
+            .then(res => setProfile(res.data.user))
+            .catch(err => {
+                msgAlert({
+                    heading: 'Error getting profiles',
+                    message: 'there has been an error, try again please',
+                    variant: 'danger'
+                })
+            })
+    }, [])
+
+
+    // Index and filter profile's goals
+    let goalCards 
+    // if there are display them
+    //else tell to make more goals
+
+    return (
+        <>
+            <Container className="m-2">
+                <Card>
+                    <Card.Header>{profile.username}</Card.Header>
+                    <Card.Body>
+                        <Card.Text>
+                            <div>
+                                Profile: {profile.username} Show Page
+                            </div>
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+
+                    </Card.Footer>
+                </Card>
+            </Container>
+            
+        </>
+    )
+}
+
+export default ShowUser
